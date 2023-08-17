7f0f48da07ce8a00d0a6efd32423b78dbecc36bd
luck22y <luckykumar2204@gmail.com>
2023-08-17 10:19:00 +0530

    init

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { Footer, Navbar } from "./components";
+
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ client/src/assets/index.js
@@ -0,0 +1,29 @@
+import NoProfile from "./userprofile.png";
+import HeroImage from "./hero.png";
+import Google from "./google.png";
+import Linkedin from "./linkedin.png";
+import Youtube from "./youtube.png";
+import WhatsApp from "./whatsapp.png";
+import Twitter from "./twitter.png";
+import Instagram from "./instagram.png";
+import Spotify from "./spotify.png";
+import Office from "./office.jpg";
+import JobImg from "./job.jpg";
+import Facebook from "./facebook.png";
+import JobFinder from "./Job-Finder-Icon.png";
+
+export {
+  JobFinder,
+  NoProfile,
+  Facebook,
+  WhatsApp,
+  Twitter,
+  Instagram,
+  Spotify,
+  HeroImage,
+  Google,
+  Linkedin,
+  Youtube,
+  Office,
+  JobImg,
+};
